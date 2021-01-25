f316275c60acd7b0d4bda5fc5e25bc33e823e1cc
Raphael Bernardo <raphaelbgr@gmail.com>
2021-01-25 09:00:36 -0300

    Android TV app hello world

---
+++ app/src/main/java/com/raphaelbgr/tvapp/mycryptopricetvapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.raphaelbgr.tvapp.mycryptopricetvapp
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+
+
+class MainActivity : FragmentActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
