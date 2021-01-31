a2e4733d35a69c53e4731977f20efcd2de791a4c
Raphael Bernardo <raphaelbgr@gmail.com>
2021-01-31 17:52:19 -0300

    + Theme definition + Created grid view + side menu with headers

--- app/src/main/java/com/raphaelbgr/tvapp/mycryptopricetvapp/MainActivity.kt
+++ app/src/main/java/com/raphaelbgr/tvapp/mycryptopricetvapp/MainActivity.kt
@@ -2,8 +2,6 @@ package com.raphaelbgr.tvapp.mycryptopricetvapp
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
 
 
 class MainActivity : FragmentActivity(R.layout.activity_main) {
--- app/src/main/java/com/raphaelbgr/tvapp/mycryptopricetvapp/MainFragment.kt
+++ app/src/main/java/com/raphaelbgr/tvapp/mycryptopricetvapp/MainFragment.kt
@@ -1,59 +1,43 @@
 package com.raphaelbgr.tvapp.mycryptopricetvapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.leanback.app.BrowseSupportFragment
+import androidx.leanback.widget.*
+import com.raphaelbgr.tvapp.mycryptopricetvapp.presenter.GridItemPresenter
+import com.raphaelbgr.tvapp.mycryptopricetvapp.presenter.IconHeaderPresenter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class MainFragment : BrowseSupportFragment() {
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MainFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MainFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var mCategoryRowAdapter: ArrayObjectAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        title = getString(R.string.app_name)
+        headersState = HEADERS_ENABLED
+        isHeadersTransitionOnBackEnabled = true
+        brandColor = ContextCompat.getColor(requireContext(), R.color.fastlane_background)
+        searchAffordanceColor = ContextCompat.getColor(requireContext(), R.color.search_opaque)
+        prepareEntranceTransition()
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MainFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MainFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        setHeaderPresenterSelector(object : PresenterSelector() {
+            override fun getPresenter(o: Any): Presenter {
+                return IconHeaderPresenter()
             }
+        })
+
+        mCategoryRowAdapter = ArrayObjectAdapter(ListRowPresenter())
+        adapter = mCategoryRowAdapter
+
+
+        // Create a row for this special case with more samples.
+        val gridHeader = HeaderItem(getString(R.string.bitcoin))
+        val gridPresenter = GridItemPresenter()
+        val gridRowAdapter = ArrayObjectAdapter(gridPresenter)
+        gridRowAdapter.add(getString(R.string.bitcoin))
+        val row = ListRow(gridHeader, gridRowAdapter)
+        mCategoryRowAdapter.add(row)
+
+        startEntranceTransition()
     }
 }
